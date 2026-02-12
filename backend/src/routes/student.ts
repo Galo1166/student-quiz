8c2b51fbea8d5e88040a8955bbf856dfaa15bc40
Muhammad Galo Aminu <163302869+Galo1166@users.noreply.github.com>
2026-02-12 17:01:32 +0100

    Delete backend directory

--- backend/src/routes/student.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllStudents } from '../controllers/studentController.js';
-import { authMiddleware, adminMiddleware } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.get('/', authMiddleware, adminMiddleware, getAllStudents);
-
-export default router;
